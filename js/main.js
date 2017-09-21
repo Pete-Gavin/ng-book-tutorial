646478ad72b89592e43188568690ddf61dd08494
Peter Gavin <littlelizard.dev@gmail.com>
2017-09-21 17:57:29 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,30 @@
+var app = angular.module("myApp", ['ngRoute']);
+
+app.config(function($routeProvider){
+	$routeProvider.when('/', {
+		templateUrl: "templates/home.html",
+		controller: 'HomeController'
+	})
+	.when('/settings', {
+		templateUrl: "templates/settings.html",
+		controller: 'SettingsController'
+	})
+	.otherwise({ redirectTo: '/' });
+});
+
+app.controller('HomeController', function($scope){
+	
+});
+
+
+app.controller('SettingsController', function($scope){
+	$scope.settings = {
+		name: "Peter",
+		email: "test@test.com"
+	}
+	
+	$scope.updateSettings = function(){
+		console.log("updateSettings was called")	
+	}
+	
+});
